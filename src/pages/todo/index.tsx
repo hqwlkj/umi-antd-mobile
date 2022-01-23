4badc03b794b960f4b1858f93310012c10569136
Yanghc <hqwlkj@parsec.com.cn>
2022-01-23 22:54:28 +0800

    fix: 修改示例代码

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,4 +1,4 @@
-import { NavBar, Toast, Result } from "antd-mobile";
+import { NavBar, Toast, Result, Space, ProgressCircle } from "antd-mobile";
 import { useHistory } from "umi";
 
 const Todo = ()=>{
@@ -16,6 +16,20 @@ const Todo = ()=>{
       title='操作成功'
       description='内容详情可折行，建议不超过两行建议不超过两行建议不超过两行'
     />
+    <div>
+      <div className="">指定线条宽度</div>
+      <Space style={{ '--gap': '24px' }}>
+        <ProgressCircle percent={75} style={{ '--track-width': '2px' }}>
+          75%
+        </ProgressCircle>
+        <ProgressCircle percent={75} style={{ '--track-width': '3px' }}>
+          75%
+        </ProgressCircle>
+        <ProgressCircle percent={75} style={{ '--track-width': '4px' }}>
+          75%
+        </ProgressCircle>
+      </Space>
+    </div>
   </div>
 }
 
