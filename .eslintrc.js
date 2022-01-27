ed20c2e95613114f59b61c081be5f3a4e65eb3ae
Yanghc <hqwlkj@parsec.com.cn>
2022-01-27 11:46:48 +0800

    fix: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-expressions': 'off',
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'import/no-anonymous-default-export': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    'react/no-array-index-key': 0,
+  },
+};
